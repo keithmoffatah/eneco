2c273055a5b75860574c0dc72d63ddd3c65e652b
keithmoffatah <keth.moffat@ah.nl>
2025-07-08 19:03:20 +0200

    state fixed

--- provider.tf
+++ provider.tf
@@ -14,6 +14,7 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
+  # Do not set use_azure_cli or any SP-specific auth here; rely on environment variables in CI/CD, and az login locally.
 }
 
 provider "databricks" {
