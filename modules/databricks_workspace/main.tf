ff03a9d58aab376b0b2aa807ac2e01f8df7f161b
keithmoffatah <keth.moffat@ah.nl>
2025-07-08 11:53:27 +0200

    fixing existing resource group and depreciated attribute

--- modules/databricks_workspace/main.tf
+++ modules/databricks_workspace/main.tf
@@ -1,12 +1,7 @@
-resource "azurerm_resource_group" "main" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
 resource "azurerm_databricks_workspace" "main" {
   name                = var.workspace_name
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  resource_group_name = var.resource_group_name
+  location            = var.location
   sku                 = "premium"
 }
 
--- modules/storage_account/main.tf
+++ modules/storage_account/main.tf
@@ -9,6 +9,6 @@ resource "azurerm_storage_account" "this" {
 
 resource "azurerm_storage_container" "this" {
   name                  = var.container_name
-  storage_account_name  = azurerm_storage_account.this.name
+  storage_account_id    = azurerm_storage_account.this.id
   container_access_type = "private"
 }
