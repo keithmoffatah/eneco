1a099981918a860bf97d470462a98f40f4f0c7d1
keithmoffatah <keth.moffat@ah.nl>
2025-07-08 19:45:45 +0200

    updated to proper node size

--- modules/databricks_workspace/main.tf
+++ modules/databricks_workspace/main.tf
@@ -1,7 +1,7 @@
 resource "databricks_cluster" "shared" {
   cluster_name            = "Shared-Cluster"
   spark_version           = "13.3.x-scala2.12"
-  node_type_id            = "Standard_D3_v2" # or "Standard_DS3_v2"
+  node_type_id            = "Standard_DC1s_v3" # or "Standard_DS3_v2"
   autotermination_minutes = 60
   num_workers             = 1
 }
