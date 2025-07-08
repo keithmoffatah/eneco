65dd259b5a89157b22a895db8b4092abffeb7eea
keithmoffatah <keth.moffat@ah.nl>
2025-07-08 12:45:18 +0200

    removed resource group from databricks_workspace

--- modules/databricks_workspace/main.tf
+++ modules/databricks_workspace/main.tf
@@ -1,8 +1,3 @@
-resource "azurerm_resource_group" "main" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
 resource "azurerm_databricks_workspace" "main" {
   name                = var.workspace_name
   resource_group_name = var.resource_group_name
