e48fd8efd532b57f7b12625cffa7533926cb3cc1
keithmoffatah <keth.moffat@ah.nl>
2025-07-08 20:16:42 +0200

    updated cluster count to 0

--- modules/databricks_workspace/main.tf
+++ modules/databricks_workspace/main.tf
@@ -1,8 +1,8 @@
 resource "databricks_cluster" "shared" {
+  count                   = 0 # Set to 1+ when clusters are allowed
   cluster_name            = "Shared-Cluster"
   spark_version           = "13.3.x-scala2.12"
   node_type_id            = "Standard_DS3_v2"
   autotermination_minutes = 60
-  // Update num_workers when not using free azure
   num_workers             = 0
 }
