6b7ca972348fc2f9301044fa9ecfaba649794f48
keithmoffatah <keth.moffat@ah.nl>
2025-07-08 20:47:19 +0200

    reverted and upgraded azure account

--- modules/databricks_workspace/main.tf
+++ modules/databricks_workspace/main.tf
@@ -1,5 +1,5 @@
 resource "databricks_cluster" "shared" {
- # count                   = 0 # Set to 1+ when clusters are allowed
+  count                   = 0 # Set to 1+ when clusters are allowed
   cluster_name            = "Shared-Cluster"
   spark_version           = "13.3.x-scala2.12"
   node_type_id            = "Standard_DS3_v2"
